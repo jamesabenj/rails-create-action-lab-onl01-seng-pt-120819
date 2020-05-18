c6169daab9a700efa6b46beefdf9279ac8f44b51
James Benjamin <james.abenj@gmail.com>
2020-05-18 17:50:12 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.create(params)
+    @student = Student.create(params[:student])
     redirect_to students_path(@student)
   end
 
